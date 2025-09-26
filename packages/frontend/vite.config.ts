2d8f8d51688944d5cba12a76613358bb13573ef1
inmare <yatsumehole@naver.com>
2025-09-26 21:50:15 +0900

    tailwind 삭제

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,9 +1,7 @@
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), flowbiteReact()],
+  plugins: [react()],
 });
